2ced80cf1d31a666762ea409dd6aeaea64835e9e
JLSC24 <jlsc24dg@gmail.com>
2018-09-20 17:16:11 -0500

    Modificación migraciones
    Se agregaron los campos que pueden ser nulos y la variable para almacenamiento de la ruta de imagen

--- database/migrations/2018_09_19_115359_create_proyecto_eventos_tbl.php
+++ database/migrations/2018_09_19_115359_create_proyecto_eventos_tbl.php
@@ -16,9 +16,9 @@ class CreateProyectoEventosTbl extends Migration
         Schema::create('proyecto', function (Blueprint $table) {
             $table->increments('id_proyecto')->primary();
             $table->dateTime('fecha_inicio');
-            $table->dateTime('fecha_final');
+            $table->dateTime('fecha_final')->nullable();
             $table->string('titulo',100);
-            $table->string('participantes');
+            $table->string('participantes')->nullable();
             $table->string('descripción');       
             $table->timestamps();
         });
